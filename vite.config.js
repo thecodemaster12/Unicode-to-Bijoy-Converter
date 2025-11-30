0aef1a12d688ca82c482aa85e8aaab220ad964d8
saifurrahman <thecodemaster26@gmail.com>
2025-11-30 11:39:40 +0600

    Fix config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-// import tailwindcss from '@tailwindcss/vite'
-import path from 'path'
+import tailwindcss from '@tailwindcss/vite'
+// import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
