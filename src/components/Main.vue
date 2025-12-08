fb7192c60b38dd22e7af7ec18e4279fbecb9b724
thecodemaster12 <thecodemaster26@gmail.com>
2025-12-08 16:26:49 +0600

    Add buttons

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -3,7 +3,7 @@
   import Greetings from './Greetings.vue'
   import Controls from './Controls.vue'
   import { ConvertToUnicode, ConvertToASCII } from '../converter';
-  import { PhClipboardText, PhBroom } from '@phosphor-icons/vue';
+  import { PhClipboardText, PhBroom, PhArrowBendLeftUp, PhArrowBendRightDown } from '@phosphor-icons/vue';
 
   const message = ref({
     title: 'Your Bangla Text Conversion Companion',
@@ -79,9 +79,15 @@
             :class="{ 'text-indigo-500': copied }" :size="26" />
         </div>
 
-        <!-- <Controls /> -->
+         <!-- <Controls /> -->
 
         <div class="w-full flex justify-center flex-wrap py-2 gap-2">
+          <button @click="bijoyToUnicode" class="control-btn flex items-center">
+              <PhArrowBendLeftUp :size="25" class="mb-4" /> Bijoy to Unicode
+          </button>
+          <button @click="unicodeToBijoy" class="control-btn flex items-center">Unicode to Bijoy
+              <PhArrowBendRightDown :size="25" class="mt-4" />
+          </button>
           <button @click="clearText" class="flex items-center gap-2 bg-red-600 hover:bg-red-500 cursor-pointer px-4 py-2 rounded text-white text-lgl">Clear Text <PhBroom :size="22" /></button>
         </div>
 
